d8dbd343ea637a835d3192bd02656e97d8dd69f1
Matthias Schiffer <mschiffer@universe-factory.net>
2018-12-16 00:29:59 +0100

    Remove parts we don't need

--- website/src/examples/basic/PercentageVertical.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SplitPane from 'react-split-pane';
-
-export default () => (
-  <SplitPane defaultSize="50%">
-    <div>default percentage: 50%</div>
-    <div />
-  </SplitPane>
-);
